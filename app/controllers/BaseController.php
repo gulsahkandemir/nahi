5852bfc1212edf7304db8e577bb8a03785482aac
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-07 01:42:45 -0700

    - Initial template structure is formed. - Misc. css / js files are linked to our default template - A proof of concept working bootstrap js is rendered at home.index

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,16 +1,16 @@
 <?php
 
 class BaseController extends Controller {
+    /** @var \Illuminate\View\View */
+    public $layout = 'layouts.default';
 
 	/**
 	 * Setup the layout used by the controller.
 	 *
 	 * @return void
 	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
+	protected function setupLayout() {
+		if(!is_null($this->layout)) {
 			$this->layout = View::make($this->layout);
 		}
 	}
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>{{ $title }}</title>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.js"></script>
+        <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css" />
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" />
+    </head>
+    <body>
+        {{ $body }}
+    </body>
+</html>
