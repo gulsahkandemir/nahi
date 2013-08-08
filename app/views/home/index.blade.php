0ee4ee09dc8861983c0a0acb1389d00158224015
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-08 01:07:15 -0700

    Final design for welcome page.

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -4,8 +4,15 @@
             &ldquo;I need a hand in&rdquo; is coming out soon!
         </h3>
         <form class="form-inline">
-            <input type="text" placeholder="e-mail">
-            <button type="submit" class="btn btn-primary">let me know</button>
+            <div class="input-prepend">
+                <span class="add-on">
+                    <i class="icon-envelope"></i>
+                </span>
+                <input type="text" placeholder="e-mail">
+                <button type="submit" class="btn btn-primary">let me know</button>
+            </div>
         </form>
     </div>
 </div>
+
+<img class="reaching-hands" src="/images/reaching-hands.png" />
