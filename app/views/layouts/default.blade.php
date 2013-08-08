633e8d243bf494edafbf363ceae78e1a7915fa33
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-07 22:43:40 -0700

    Initial welcome page + less structure

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,15 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>{{ $title }}</title>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/webfont/1.4.8/webfont.js"></script>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.js"></script>
-        <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.js"></script>
+        <script type="text/javascript" src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.js"></script>
 
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css">
         <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css" />
-        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.css" />
         <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" />
+
+        <link rel="stylesheet" type="text/css" href="/style/site.css">
     </head>
     <body>
-        {{ $body }}
+        <div class="container">
+            {{ $body }}
+        </div>
     </body>
 </html>
