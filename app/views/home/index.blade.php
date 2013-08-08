4f57f72c7d05d60529a9d525e1902b0131903cad
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-07 23:40:24 -0700

    Center welcome-container

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,11 +1,11 @@
 <div class="row text-center">
-    <div class="span8 offset2">
+    <div class="span8 offset2 welcome-container">
         <h3>
             &ldquo;I need a hand in&rdquo; is coming out soon!
         </h3>
         <form class="form-inline">
             <input type="text" placeholder="e-mail">
-            <button type="submit" class="btn btn-primary">Let me know</button>
+            <button type="submit" class="btn btn-primary">let me know</button>
         </form>
     </div>
 </div>
