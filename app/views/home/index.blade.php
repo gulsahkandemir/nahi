e8a0a5b6eab026e0dee811c872359e57898f793d
Gulsah Kandemir Yildiz <fgulsahkandemir@gmail.com>
2013-08-07 18:00:43 -0700

    The initial email retrieve page (index) is updated, but has flaws.
    Signed-off-by: Gulsah Kandemir Yildiz <fgulsahkandemir@gmail.com>

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,21 +1,33 @@
-<!-- Button trigger modal -->
-<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Launch demo modal</a>
+<!---->
+<!--<!-- Button trigger modal -->
+<!--<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Launch demo modal</a>-->
+<!---->
+<!--<!-- Modal -->
+<!--<div class="modal fade" id="myModal">-->
+<!--    <div class="modal-dialog">-->
+<!--        <div class="modal-content">-->
+<!--            <div class="modal-header">-->
+<!--                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>-->
+<!--                <h4 class="modal-title">Modal title</h4>-->
+<!--            </div>-->
+<!--            <div class="modal-body">-->
+<!--                ...-->
+<!--            </div>-->
+<!--            <div class="modal-footer">-->
+<!--                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
+<!--                <button type="button" class="btn btn-primary">Save changes</button>-->
+<!--            </div>-->
+<!--        </div><!-- /.modal-content -->
+<!--    </div><!-- /.modal-dialog -->
+<!--</div><!-- /.modal -->
 
-<!-- Modal -->
-<div class="modal fade" id="myModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Modal title</h4>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+<h3 class="text-center"><span class="label label-default" >I Need a Hand In is coming out soon! To get informed earlier, leave your e-mail address !</span></h3>
+<form>
+<fieldset>
+<!--      <legend class="">I Need a Hand In is coming out soon! To get informed earlier, leave your e-mail address !</legend> -->
+        <div class="col-lg-4 col-lg-offset-10">
+            <input type="text" class="form-control " id="exampleInputEmail" placeholder="Enter email">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+</fieldset>
+</form>
