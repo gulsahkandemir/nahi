354a05f4103dbb325bcd9eeca2eb5f9d32d373c6
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-15 01:19:48 -0700

    Commit meta tags for Facebook and twitter

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,6 +3,26 @@
     <head>
         <title>{{$title}} | Need a Hand</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:url" content="http://needahand.in">
+        <meta name="twitter:title" content="Coming Soon! | Need a Hand">
+        <meta name="twitter:description" content="We're helping your friends to help you.
+                Do you need help to organize a big baby shower?
+                Or do you simply want one of your handy friends to help fix your sink?
+                Just ask for hand using Need a Hand.">
+        <meta property="twitter:image" content="http://needahand.in/images/nahi-og-image.png"/>
+
+        <meta property="og:image" content="http://needahand.in/images/nahi-og-image.png"/>
+        <meta property="og:title" content="Coming Soon! | Need a Hand"/>
+        <meta property="og:description" content="We're helping your friends to help you.
+                Are you moving to a new place? Do you need help to organize a big baby shower?
+                Or do you simply want one of your handy friends to help fix your sink?
+                Just ask for hand using 'Need a Hand'."/>
+        <meta property="og:url" content="http://needahand.in"/>
+        <meta property="og:site_name" content="Need a Hand"/>
+        <meta property="og:type" content="website"/>
+
         <link rel="stylesheet" type="text/css" href="/style/main.css">
         <script data-main="/scripts/main.js" src="/vendor/requirejs/require.js"></script>
     </head>
