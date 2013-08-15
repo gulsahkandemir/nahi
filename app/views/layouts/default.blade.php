aa97c226cf15f54c2013c5df83c800469722feb6
Cansin Yildiz <cansinyildiz@gmail.com>
2013-08-15 00:29:39 -0700

    Change js folder's name to scripts

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
         <title>{{$title}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="stylesheet" type="text/css" href="/style/main.css">
-        <script data-main="/js/main.js" src="/vendor/requirejs/require.js"></script>
+        <script data-main="/scripts/main.js" src="/vendor/requirejs/require.js"></script>
     </head>
     <body class="{{$route}}">
         <div class="container">
